b14364bcfa78ad057407e77405ee48facb9a8d5c
Meghna Gururani <149100091+meghna-gururani@users.noreply.github.com>
2024-09-17 02:02:18 +0530

    Time: 3 ms (17.23%), Space: 7.4 MB (49.82%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -5,8 +5,6 @@ public:
         int rem=0;
         while(numBottles>0){
             int quo=(numBottles+rem)/numExchange;
-            if(quo==0)
-              break;
             s+=quo;
             rem=(numBottles+rem)%numExchange;
             numBottles=quo;
