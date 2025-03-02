f1a87b2566d7628d155aa453bc444629b9537183
Meghna Gururani <149100091+meghna-gururani@users.noreply.github.com>
2025-03-02 16:22:58 +0530

    Time: 0 ms (100%), Space: 15.2 MB (28.71%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        int i=0,j=0,k=0;
+        map<int,int>merged;
+        for(int i=0;i<nums1.size();i++){
+            merged[nums1[i][0]]+=nums1[i][1];
+        }
+         for(int i=0;i<nums2.size();i++){
+            merged[nums2[i][0]]+=nums2[i][1];
+        }
+        vector<vector<int>> result;
+        for (auto &entry : merged) {
+            result.push_back({entry.first, entry.second});
+        }
+        return result;
+    }
+
+
+      /*  int i=0,j=0,k=0;
         vector<vector<int>>res;
         while(i<nums1.size() && j<nums2.size()){
             if(nums1[i][0]==nums2[j][0]){
@@ -27,5 +42,5 @@ public:
                 j++;
     }
     return res;
- }
+ } */
 };
