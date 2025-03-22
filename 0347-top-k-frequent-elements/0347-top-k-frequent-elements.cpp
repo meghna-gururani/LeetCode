2026904d9d721b389da80dabb7f69a9d9b734008
Meghna Gururani <149100091+meghna-gururani@users.noreply.github.com>
2025-03-22 11:36:03 +0530

    Time: 0 ms (100%), Space: 17.9 MB (47.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,18 +3,20 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>freq;
         for(int num:nums){
-            freq[num]++;
+          freq[num]++;
         }
-        vector<pair<int,int>>res(freq.begin(),freq.end());
-        sort(res.begin(),res.end(),[](auto &a,auto &b){
-            return a.second>b.second;
-        });
-        vector<int>ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(res[i].first);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>minheap;
+        for(auto &[num,count]:freq){
+            minheap.push({count,num});
+            if(minheap.size()>k){
+                minheap.pop();
+            }
         }
-        return ans;
+        vector<int>res;
+        while(!minheap.empty()){
+            res.push_back(minheap.top().second);
+            minheap.pop();
+        }
+        return res;
     }
-
-
 };
