cc10238dcfd030ca62c9205e2968432e6ef9891e
Meghna Gururani <149100091+meghna-gururani@users.noreply.github.com>
2025-03-22 11:11:27 +0530

    Time: 0 ms (100%), Space: 17.9 MB (71.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<int>freqq;
+        unordered_map<int,int>freq;
+        for(int num:nums){
+            freq[num]++;
+        }
+        vector<pair<int,int>>res(freq.begin(),freq.end());
+        sort(res.begin(),res.end(),[](auto &a,auto &b){
+            return a.second>b.second;
+        });
         vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==-2) continue;
-            int freq=1;
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    freq++;
-                    nums[j]=-2;
-                }
-            }
-            freqq.push_back(freq);
-            ans.push_back(nums[i]);
+        for(int i=0;i<k;i++){
+            ans.push_back(res[i].first);
         }
-        for(int i=0;i<freqq.size()-1;i++){
-            for(int j=0;j<freqq.size()-i-1;j++){
-                if(freqq[j]<freqq[j+1]){
-                    swap(freqq[j],freqq[j+1]);
-                    swap(ans[j],ans[j+1]);
-                }
-            }
-        }
-        return vector<int>(ans.begin(),ans.begin()+k);
+        return ans;
     }
+
+
 };
